2f3597864f0869892c07faf6676d3d4de1a9b669
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-10-05 22:44:46 -0500

    adding home page and route

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const sequelize = require("./config/config");
-const models = require("./models");
 
- 
-app.listen(PORT, async () => {
-   console.log(`App listening on port ${PORT}!`);
-   try {
-    await sequelize.sync({ force: true })
-       console.log('connected to db!')
-   } catch(e){
-       console.log(e)
-   }
- });
+// Set up sessions
+const sess = {
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({helpers});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
+});
