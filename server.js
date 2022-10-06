be9f17b17713b3e8682fecf60b916b523533e7af
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-10-05 21:11:22 -0500

    adding express and sql

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sequelize = require("./config/config");
+const models = require("./models");
+
+ 
+app.listen(PORT, async () => {
+   console.log(`App listening on port ${PORT}!`);
+   try {
+    await sequelize.sync({ force: true })
+       console.log('connected to db!')
+   } catch(e){
+       console.log(e)
+   }
+ });
